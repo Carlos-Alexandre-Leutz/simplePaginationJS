6994b6517da0103ef5397cb27caa0a535ff34182
Carlos Alexandre Leutz <54986289+Carlos-Alexandre-Leutz@users.noreply.github.com>
2021-07-14 14:06:55 -0300

    Create pagination.js

---
+++ pagination.js
@@ -0,0 +1,22 @@
+
+    function listItems(items, pageActual, limitItems) {
+      let result = [];
+      let totalPage = Math.ceil(items.length / limitItems);
+      let count = pageActual * limitItems - limitItems;
+      let delimiter = count + limitItems;
+      if (pageActual <= totalPage) {
+        for (let i = count; i < delimiter; i++) {
+          if (items[i] != null) {
+            result.push(items[i]);
+          }
+          count++;
+        }
+      }
+      return result;
+    }
+     // Exemplos de uso:
+    var next = ['Next 1','Next 2','Next 3','Next 4','Next 5'];
+     var resultNext = listItems(next, 1, 2);
+    //  var resultNext2 = listItems(next, 2, 2);
+    // var resultNext3 = listItems(next, 3, 2);   
+    console.log(resultNext);
